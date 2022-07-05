97988d8ccae8a32f33077cc3ce076c2e070a1347
Ubayd Khan <ubaydkh@gmail.com>
2022-07-05 01:07:19 +0100

    temporary patch to fix crash when morphing in a shrunken state, with Shrink mod

--- src/main/java/de/budschie/bmorph/api_interact/ShrinkAPIInteract.java
+++ src/main/java/de/budschie/bmorph/api_interact/ShrinkAPIInteract.java
@@ -6,6 +6,7 @@ import net.gigabit101.shrink.api.IShrinkProvider;
 import net.gigabit101.shrink.api.ShrinkAPI;
 //import net.gigabit101.shrink.api.IShrinkProvider;
 //import net.gigabit101.shrink.api.ShrinkAPI;
+import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.player.Player;
 
 public class ShrinkAPIInteract implements IShrinkAPIInteract
@@ -17,8 +18,17 @@ public class ShrinkAPIInteract implements IShrinkAPIInteract
 		
 		if(provider.isPresent())
 		{
-			if(provider.get().isShrunk())
-				return provider.get().defaultEntitySize().height / 0.2F;
+			if(provider.get().isShrunk()) {
+				if (provider.get().defaultEntitySize() == null) {
+					return 1;
+				}
+				EntityDimensions ed = provider.get().defaultEntitySize();
+				if (ed.height == 0.0f) {
+					ed = new EntityDimensions(1.0f, 1.0f, false);
+				}
+				System.out.println(ed);
+				return ed.height / 0.2F;
+			}
 		}
 		
 		return 1;
